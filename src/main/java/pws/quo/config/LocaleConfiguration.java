0418aa03613d6a592494a4573f1dac71bedc3cef
unknown <petar.lakcevic@gmail.com>
2023-07-04 17:49:00 +0200

    maybe fix rec

--- src/main/java/pws/quo/config/LocaleConfiguration.java
+++ src/main/java/pws/quo/config/LocaleConfiguration.java
@@ -17,11 +17,18 @@ public class LocaleConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/{spring:\\w+}")
+//            .setViewName("forward:/");
+//        registry.addViewController("/**/{spring:\\w+}")
+//            .setViewName("forward:/");
+//        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+//            .setViewName("forward:/");
+
         registry.addViewController("/{spring:\\w+}")
             .setViewName("forward:/");
         registry.addViewController("/**/{spring:\\w+}")
             .setViewName("forward:/");
-        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+        registry.addViewController("/{spring:^(?!.*\\.(js|css)$).*}/**")
             .setViewName("forward:/");
     }
 
