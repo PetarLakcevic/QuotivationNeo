ab78326a9fa854e30e427b98e466aa75926f3ca0
unknown <petar.lakcevic@gmail.com>
2023-07-04 17:22:11 +0200

    fix

--- src/main/java/pws/quo/config/LocaleConfiguration.java
+++ src/main/java/pws/quo/config/LocaleConfiguration.java
@@ -4,8 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import tech.jhipster.config.locale.AngularCookieLocaleResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
 
 @Configuration
 public class LocaleConfiguration implements WebMvcConfigurer {
@@ -22,15 +27,17 @@ public class LocaleConfiguration implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
-        AngularCookieLocaleResolver cookieLocaleResolver = new AngularCookieLocaleResolver();
-        cookieLocaleResolver.setCookieName("NG_TRANSLATE_LANG_KEY");
-        return cookieLocaleResolver;
+        return new FixedLocaleResolver(Locale.US);
+//        AngularCookieLocaleResolver cookieLocaleResolver = new AngularCookieLocaleResolver();
+//        cookieLocaleResolver.setCookieName("NG_TRANSLATE_LANG_KEY");
+//        return cookieLocaleResolver;
     }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
-        localeChangeInterceptor.setParamName("language");
-        registry.addInterceptor(localeChangeInterceptor);
-    }
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//
+//        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+//        localeChangeInterceptor.setParamName("language");
+//        registry.addInterceptor(localeChangeInterceptor);
+//    }
 }
