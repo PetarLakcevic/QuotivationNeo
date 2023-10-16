6b17b381780f451c2291114ae13005cc99656a49
unknown <petar.lakcevic@gmail.com>
2023-10-16 15:12:57 +0200

    fix

--- src/main/java/pws/quo/config/SecurityConfiguration.java
+++ src/main/java/pws/quo/config/SecurityConfiguration.java
@@ -75,6 +75,7 @@ public class SecurityConfiguration {
             .antMatchers("/api/**").authenticated()
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/health/**").permitAll()
+            .antMatchers("/api/payment-process").permitAll()
             .antMatchers("/management/info").permitAll()
             .antMatchers("/management/prometheus").permitAll()
             .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
