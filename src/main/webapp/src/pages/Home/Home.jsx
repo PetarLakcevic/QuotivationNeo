7708e78db24c43e0d2de2eb885bcf879d70c377a
Nikola Jankovic <80163534+NikolaJankovic-dev@users.noreply.github.com>
2023-10-24 01:34:15 +0200

    Update Home.jsx

--- src/main/webapp/src/pages/Home/Home.jsx
+++ src/main/webapp/src/pages/Home/Home.jsx
@@ -287,7 +287,7 @@ const Home = () => {
                       color: '#fff',
                     },
                   }}
-                  onPointerDown={() => navigate('Login')}
+                  onPointerDown={() => navigate('register')}
                   // disabled={!agree}
                 >
                   {' '}
